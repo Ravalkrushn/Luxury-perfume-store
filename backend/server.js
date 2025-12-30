f6439e64cf03d3f875660cf10977dd87aa3af054
Ravalkrushn <ravalkrishna0313@gmail.com>
2025-12-30 11:15:58 +0530

    updated ui for admin panel and home navbar

--- backend/server.js
+++ backend/server.js
@@ -9,23 +9,21 @@ const dashboardRoutes = require("./routes/dashboard.routes");
 
 const app = express();
 
-// MIDDLEWARE
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
-// ROUTES
+
 app.use("/api/auth", authRoutes);
 app.use("/api/perfumes", perfumeRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 
-// DATABASE
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error("Mongo Error:", err));
 
-// SERVER
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
